32da2675caaecd03a827166baf10d23cb57763a2
Anurag Pokhriyal <101653550+anurag174174@users.noreply.github.com>
2024-09-27 13:12:35 +0530

    Delete job portal/backend directory

--- job portal/backend/routes/user.route.js
+++
@@ -1,6 +0,0 @@
-const router=express.Router();
-router.route('/register').post(register);
-router.route('/login').post(login);
-router.route('/profile/update').post(isAuthenticated,updateProfile);
-
-export default router;
